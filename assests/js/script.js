7846f4224689892f97dc499ddc0990651c07112c
Optical23 <40125489+Optical23@users.noreply.github.com>
2021-11-14 01:49:00 -0600

    added comments of the steps to build the site

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,7 +1,44 @@
 start = document.querySelector("#startbutton");
-var buttonContainer = document.querySelector("#buttonContainer")
+var buttonContainer = document.querySelector("#buttonContainer");
+startPage = document.querySelector("#startpage");
+var currenttime = document.querySelector("timer");
+var timeleft = 0;
+/////created elements for question building
+////var of h1
+////vars of buttons
+
 
 start.addEventListener("click", function(){
+    //Remove whats on the screen to start game
+    
+    startPage.setAttribute("style", "display:none;");
+    
+    //timer that counts for score
+    timeleft = 70;
+    timerScore();
+    
+    //timer begins and then this loops till done
+    //Function that builds a question
+    /////function(){
+        //
+    //}
+    //listening for the answer removes question 
+
+
+    //builds next question one
     var answer1 = document.createElement("button");
     buttonContainer.appendChild(answer1);
+    
 });
+
+function timerScore() {
+    var countdown = setInterval(function() {
+        if(timeleft > 0) {
+            timeleft--;
+            timer.textContent = timeleft;
+        }else{
+            clearInterval(countdown);
+            // finishscreen();
+        }
+    }, 1000);
+}
